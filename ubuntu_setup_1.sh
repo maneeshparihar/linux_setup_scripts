5e144ec1f460866741d38de114c31926347b24d1
Maneesh Parihar <maneeshparihar@gmail.com>
2025-05-09 00:44:32 +0530

    adding pyenv and openssh server

--- ubuntu_setup_1.sh
+++ ubuntu_setup_1.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade -y
 # setting date time zone to Asia/Kolkata, feel free to change it 
 sudo timedatectl set-timezone Asia/Kolkata
 # installing packages 
-sudo apt install zip unzip curl  -y
+sudo apt install zip unzip curl openssh-server -y
 # installing docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
@@ -20,3 +20,14 @@ sudo ./aws/install
 rm -rf ./aws
 rm -f awscliv2.zip
 rm ./get-docker.sh
+# -----------------------------------
+# pyenv build dependencies 
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+# pyenv install 
+curl -fsSL https://pyenv.run | bash
+# copy the output into ~/.bashrc at end and restart shell 
+# pyenv install python 3.11 
+pyenv install 3.11 
+pyenv global 3.11 
